0f7c7f3e98d6df54be777d801dc577002b6dce62
Baz <111312411+Barryk112@users.noreply.github.com>
2023-03-20 10:28:48 +0000

    Updated game.test.js to include more tests and updated game accordingly

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,9 +1,9 @@
-
 /**
  * @jest-environment jsdom
  */
 
 const { game } = require("../game");
+
 
 beforeAll(() => {
     let fs = require("fs");
@@ -16,5 +16,17 @@ beforeAll(() => {
 describe("game object contains correct keys", () => {
     test("score key exists", () => {
         expect("score" in game).toBe(true);
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
 });
